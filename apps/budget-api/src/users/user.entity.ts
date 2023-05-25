4d16eeb8069baa3380263858c05c01a5dfd177d6
Dominik Drozd <dominik_drozd@vp.pl>
2023-05-25 22:54:28 +0200

    Added user entity and users providers.

---
+++ apps/budget-api/src/users/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { UserRole } from "./types";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({
+        length: 30,
+    })
+    name: string;
+
+    @Column({
+        length: 50,
+    })
+    email: string;
+
+    @Column({
+        length: 128,
+    })
+    passwordHash: string;
+
+    @Column({
+        type: "enum",
+        enum: UserRole,
+        default: UserRole.User
+    })
+    role: UserRole
+}
