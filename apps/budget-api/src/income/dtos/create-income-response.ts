f04751339fa4aeba9ed5498c04d29b9dadb5bbc1
Dominik Drozd <dominik_drozd@vp.pl>
2023-07-03 06:20:30 +0200

    Refactored data structure returned after adding income.

--- apps/budget-api/src/income/dtos/create-income-response.ts
+++ apps/budget-api/src/income/dtos/create-income-response.ts
@@ -1,6 +1,19 @@
-import { Expose } from "class-transformer";
+import { Expose, Transform } from "class-transformer";
 
 export class CreateIncomeResponse {
     @Expose()
     id: string
+
+    @Expose()
+    name: string
+
+    @Expose()
+    amount: number
+
+    @Expose()
+    date: Date
+
+    @Transform(({ obj }) => obj.user.id)
+    @Expose()
+    userId: string
 }
