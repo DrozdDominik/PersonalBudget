18ec882962517ece2580c8d2c676e5df187ec6f1
Dominik Drozd <dominik_drozd@vp.pl>
2023-07-16 17:16:11 +0200

    [Feature] AdminGuard

---
+++ apps/budget-api/src/guards/AdminGuard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { User } from "../user/user.entity";
+import { UserRole } from "../user/types";
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+    canActivate(
+        context: ExecutionContext,
+    ): boolean {
+        const user: User = context.switchToHttp().getRequest().user
+
+        return user.role === UserRole.Admin
+    }
+}
