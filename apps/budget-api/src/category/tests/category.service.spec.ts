8ca31997c3fd5f51dbf11db870fcfa04b6e86976
Dominik Drozd <dominik_drozd@vp.pl>
2023-07-19 22:53:31 +0200

    [Tests/Add] Category service

--- apps/budget-api/src/category/tests/category.service.spec.ts
+++ apps/budget-api/src/category/tests/category.service.spec.ts
@@ -1,14 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from '../category.service';
-import { describe, beforeEach, it, expect } from 'vitest'
+import {describe, beforeEach, it, expect, vi} from 'vitest'
 import { CategoryController } from "../category.controller";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { Category } from "../category.entity";
 import { Repository } from "typeorm";
+import {CreateCategoryDto} from "../dtos/create-category.dto";
+import {faker} from "@faker-js/faker";
+import {CategoryCreateData} from "../types";
+import {BadRequestException} from "@nestjs/common";
 
 describe('CategoryService', () => {
   let service: CategoryService;
   let repo: Repository<Category>
+
+  const testData: CreateCategoryDto = {
+    name: faker.word.noun()
+  }
+
+  const testDefaultCategory: Category = {
+    id: faker.string.uuid(),
+    name: faker.word.noun(),
+    isDefault: true,
+    user: null,
+    incomes: []
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,7 +34,9 @@ describe('CategoryService', () => {
         {
           provide: getRepositoryToken(Category),
           useValue: {
-
+            create: vi.fn(),
+            save: vi.fn(),
+            findOne: vi.fn(),
           },
         }
       ],
@@ -32,4 +50,26 @@ describe('CategoryService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Create default method', () => {
+    it('should call categoryRepository.create method with correct data', async () => {
+      vi.spyOn(repo, 'findOne').mockResolvedValueOnce(null)
+
+      const dataToSave: CategoryCreateData = {
+        name: testData.name,
+        isDefault: true,
+        user: null
+      }
+
+      await service.createDefault(testData)
+
+      expect(repo.create).toHaveBeenCalledWith(dataToSave)
+    })
+
+    it('should throw error if category already exists', async () => {
+      vi.spyOn(repo, 'findOne').mockResolvedValueOnce(testDefaultCategory)
+
+      await expect(service.createDefault(testData)).rejects.toThrowError(BadRequestException)
+    })
+  })
 });
