f2d1f6feae5768967769abeff20b23e854630260
Dominik Drozd <dominik_drozd@vp.pl>
2023-09-21 09:20:25 +0200

    [Tests][Add] Transaction controller tests

--- apps/budget-api/src/transaction/tests/transaction.controller.spec.ts
+++ apps/budget-api/src/transaction/tests/transaction.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { TransactionController } from '../transaction.controller'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { TransactionService } from '../transaction.service'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Transaction } from '../transaction.entity'
@@ -11,9 +11,24 @@ import { BudgetService } from '../../budget/budget.service'
 import { UserService } from '../../user/user.service'
 import { Budget } from '../../budget/budget.entity'
 import { User } from '../../user/user.entity'
+import { INestApplication } from '@nestjs/common'
+import { userFactory } from '../../user/tests/utlis'
+import { AuthGuard } from '@nestjs/passport'
+import { NextFunction, Request, Response } from 'express'
+import { CreateTransactionDto } from '../dtos/create-transaction.dto'
+import { TransactionType } from '../types'
+import { transactionFactory } from './utils'
+import request from 'supertest'
+import { TransactionResponse } from '../dtos/transaction-response'
+import { EditTransactionDto } from '../dtos/edit-transaction.dto'
+import { faker } from '@faker-js/faker'
 
 describe('TransactionController', () => {
   let controller: TransactionController
+  let service: TransactionService
+  let app: INestApplication
+
+  const [loggedUser] = userFactory()
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -40,12 +55,146 @@ describe('TransactionController', () => {
           useClass: Repository,
         },
       ],
-    }).compile()
+    })
+      .overrideGuard(AuthGuard('jwt'))
+      .useValue(true)
+      .compile()
 
     controller = module.get<TransactionController>(TransactionController)
+
+    service = module.get<TransactionService>(TransactionService)
+
+    app = module.createNestApplication()
+
+    app.use((req: Request & { user: User }, res: Response, next: NextFunction) => {
+      req.user = loggedUser
+      next()
+    })
+
+    await app.init()
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('POST endpoint', () => {
+    it('should return 201 code and TransactionResponse', () => {
+      const [newTransaction] = transactionFactory(1, TransactionType.INCOME)
+      const newTransactionData: CreateTransactionDto = {
+        categoryId: newTransaction.category.id,
+        type: newTransaction.type,
+        amount: newTransaction.amount,
+        date: newTransaction.date,
+        budgetId: newTransaction.budget.id,
+        comment: newTransaction.comment,
+      }
+      const expectedResponse: TransactionResponse = {
+        id: newTransaction.id,
+        type: newTransaction.type,
+        category: newTransaction.category.name,
+        amount: newTransaction.amount,
+        date: newTransaction.date.toISOString(),
+        budgetId: newTransaction.budget.id,
+        comment: newTransaction.comment,
+        userId: newTransaction.user.id,
+      }
+
+      vi.spyOn(service, 'create').mockResolvedValueOnce(newTransaction)
+
+      return request(app.getHttpServer())
+        .post('/transaction')
+        .send(newTransactionData)
+        .expect(201)
+        .expect(expectedResponse)
+    })
+  })
+
+  describe('PATCH endpoint', () => {
+    it('should return 200 code and TransactionResponse', () => {
+      const editedData: EditTransactionDto = {
+        type: TransactionType.EXPENSE,
+        amount: Number(faker.finance.amount(0, 1000000, 2)),
+      }
+
+      const [transaction] = transactionFactory(1, TransactionType.INCOME)
+
+      const editedTransaction: Transaction = {
+        ...transaction,
+        ...editedData,
+      }
+
+      const expectedResponse: TransactionResponse = {
+        id: editedTransaction.id,
+        type: editedTransaction.type,
+        category: editedTransaction.category.name,
+        amount: editedTransaction.amount,
+        date: editedTransaction.date.toISOString(),
+        budgetId: editedTransaction.budget.id,
+        comment: editedTransaction.comment,
+        userId: editedTransaction.user.id,
+      }
+
+      vi.spyOn(service, 'edit').mockResolvedValueOnce(editedTransaction)
+
+      return request(app.getHttpServer())
+        .patch(`/transaction/${transaction.id}`)
+        .send(editedData)
+        .expect(200)
+        .expect(expectedResponse)
+    })
+  })
+
+  describe('DELETE endpoint', () => {
+    it('should return 204 code', () => {
+      const [transaction] = transactionFactory(1, TransactionType.INCOME)
+      vi.spyOn(service, 'delete').mockImplementationOnce(async () => {})
+      return request(app.getHttpServer()).delete(`/transaction/${transaction.id}`).expect(204)
+    })
+  })
+
+  describe('GET "/" endpoint', () => {
+    it('should return 200 code and TransactionResponse', () => {
+      const transactions = transactionFactory(5, TransactionType.INCOME)
+      const expectedResponse: TransactionResponse[] = transactions.map(transaction => ({
+        id: transaction.id,
+        type: transaction.type,
+        category: transaction.category.name,
+        amount: transaction.amount,
+        date: transaction.date.toISOString(),
+        budgetId: transaction.budget.id,
+        comment: transaction.comment,
+        userId: transaction.user.id,
+      }))
+      vi.spyOn(service, 'getAll').mockResolvedValueOnce(transactions)
+
+      return request(app.getHttpServer()).get('/transaction').expect(200).expect(expectedResponse)
+    })
+  })
+
+  describe('GET "/:id" endpoint', () => {
+    it('should return 200 code and TransactionResponse', () => {
+      const [transaction] = transactionFactory(1, TransactionType.INCOME)
+      const expectedResponse: TransactionResponse = {
+        id: transaction.id,
+        type: transaction.type,
+        category: transaction.category.name,
+        amount: transaction.amount,
+        date: transaction.date.toISOString(),
+        budgetId: transaction.budget.id,
+        comment: transaction.comment,
+        userId: transaction.user.id,
+      }
+      vi.spyOn(service, 'getOne').mockResolvedValueOnce(transaction)
+
+      return request(app.getHttpServer())
+        .get(`/transaction/${transaction.id}`)
+        .expect(200)
+        .expect(expectedResponse)
+    })
   })
 })
